55dc653a6adf2d035ef2fd62c08045ee714dd9bd
trungnghia0122 <tonytran0122@gmail.com>
2024-04-04 20:48:03 -0500

    new update

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,52 +1,5 @@
 'use client'
 
-// import { useState, useEffect } from 'react';
-// import { DocumentData } from 'firebase/firestore';
-
-// type Props = {
-//     message: DocumentData;
-// }
-
-// function Message({ message }: Props) {
-//     const [currentWordIndex, setCurrentWordIndex] = useState(0);
-//     const words = message.text.split(' ');
-//     const isChatGPT = message.user.name === "ChatGPT";
-
-//     useEffect(() => {
-//         if (isChatGPT && currentWordIndex < words.length) {
-//             const timer = setTimeout(() => {
-//                 setCurrentWordIndex(currentWordIndex + 1);
-//             }, 200);
-
-//             return () => clearTimeout(timer);
-//         }
-//     }, [isChatGPT, currentWordIndex, words]);
-
-//     return (
-//         <div className="text-white py-4">
-//             <div className="flex flex-col space-x-5 max-w-2xl mx-auto">
-//                 <div className="flex space-x-2">
-//                     <img src={message.user.avatar} alt="user" className="h-6 w-6 rounded-full" />
-//                     <p className="font-bold">{isChatGPT ? "ChatGPT" : "You"}</p>
-//                 </div>
-
-//                 <div className='text-sm px-3 leading-6'>
-//                     {!isChatGPT && (
-//                         <p className="text-sm px-3 leading-6">{message.text}</p>
-//                     )}
-
-//                     {isChatGPT && (
-//                         words.slice(0, currentWordIndex).join(' ')
-//                     )}
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Message;
-
-import { useState, useEffect, useMemo } from 'react';
 import { DocumentData } from 'firebase/firestore';
 
 type Props = {
@@ -54,8 +7,6 @@ type Props = {
 }
 
 function Message({ message }: Props) {
-    const [currentWordIndex, setCurrentWordIndex] = useState(0);
-    const words = useMemo(() => message.text.split(' '), [message.text]);
     const isChatGPT = message.user.name === "ChatGPT";
 
     return (
@@ -65,11 +16,8 @@ function Message({ message }: Props) {
                     <img src={message.user.avatar} alt="user" className="h-6 w-6 rounded-full" />
                     <p className="font-bold">{isChatGPT ? "ChatGPT" : "You"}</p>
                 </div>
-
                 <div className='text-sm px-3 leading-6'>
-  
                         <p>{message.text}</p>
-
                 </div>
             </div>
         </div>
